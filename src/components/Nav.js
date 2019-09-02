d2f77d75b48d594e9a69e0be418432c7c28649f4
Emilio Sanchez <sanchez.emilio483@gmail.com>
2019-09-02 16:15:00 -0500

    styled nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,11 +4,11 @@ import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
   navButton: {
-    background: 'linear-gradient(45deg, #BF94E4 30%, #967bb6 90%)',
+    background: 'linear-gradient(45deg, #FFFF00 30%, #e1ffb3 90%)',
     border: 0,
     borderRadius: 3,
     boxShadow: '0 1px 5px 1px rgba(200, 100, 200, .3)',
-    color: 'white',
+    color: '#BF94E4',
     height: 40,
     padding: '0 20px',
   }
@@ -17,9 +17,13 @@ const useStyles = makeStyles({
 export default function Nav() {
   const classes = useStyles();
   return (
-    <div>
-      <h1>Generic Site</h1>
-      <Button className={classes.navButton}>About</Button>
+    <div className='nav'>
+      <div className='titleNav'>
+        <h1>Generic Site</h1>
+      </div>
+      <div className='aboutButtonNav'>
+              <Button className={classes.navButton}>About</Button>
+      </div>
     </div>
   )
 }
