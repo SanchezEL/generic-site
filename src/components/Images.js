c7617b7154cc0dcb1ae4b30dc48208eccf1b1d28
Emilio Sanchez <sanchez.emilio483@gmail.com>
2019-09-02 20:46:37 -0500

    added slideshow

--- src/components/Images.js
+++ src/components/Images.js
@@ -3,22 +3,40 @@ import React, { Component } from 'react'
 export default class Images extends Component {
   constructor(props){
     super(props)
+    this.state = {
+      slideIndex: 0
+    }
+  }
+  
+  changeIndex(n){
+    console.log(n)
+    let tempIndex = this.state.slideIndex + n
+    if(tempIndex > this.props.images.length-1){
+      tempIndex = 0;
+    }
+    if(tempIndex < 0){
+      tempIndex = this.props.images.length-1
+    }
+    if(tempIndex !== this.state.slideIndex){
+      console.log(this.state)
+      this.setState({slideIndex: tempIndex})
+    }
+    console.log('changing index')
   }
   render() {
     console.log('props in images', this.props)
     return (
       <div>
-        {
-          this.props.images.map((image,i) => {
-            console.log('this image: ', image)
-            return(
-              <div key={i}>
-                <img src={image}/>
-              </div>
-            )
-          })
-        }
+        <div className="slideshow-container">
+          <div className="slides fade">
+            <img src={this.props.images[this.state.slideIndex]} height='200' width='200'/>
+          </div>
+        </div>
+
+        <a className="prev" onClick={() => {this.changeIndex(-1)}}>❮</a>
+        <a className="next" onClick={() => {this.changeIndex(1)}}>❯</a>
       </div>
     )
   }
 }
+
